a6b153c6826b3b43552d3aea145ed43a2ce4ecde
ampandey <amritpandey2005@gmail.com>
2026-08-08 18:18:23 +0530

    revert some changes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -28,5 +29,14 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/health", include_in_schema=False)
-def read_root():
-    return {"message": "health check", "status": "ok"}
+async def health_check():
+    return {
+        "status": "healthy"
+    }
+
+# only verify the all three fastapi instances that we define in docker-compose for load balancing
+@app.get("/instance", include_in_schema=False)
+async def get_instance():
+    return {
+        "hostname": os.getenv("HOSTNAME")
+    }
