4b6a8c34ad0ad48975347f5140794e7d414de8ba
ampandey <amritpandey2005@gmail.com>
2026-08-15 02:08:17 +0530

    create get all users api

--- app/api/v1/endpoints/admin.py
+++ app/api/v1/endpoints/admin.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.db.database import get_db
 from app.models.user import User
 
 router = APIRouter()
@@ -11,3 +14,16 @@ async def test_admin():
     return {
         "message": "Admin access granted",
     }
+
+@router.get("/users")
+async def get_users(db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(User))
+    users = result.scalars().all()
+
+    if not users:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail="No users yet"
+        )
+
+    return users
